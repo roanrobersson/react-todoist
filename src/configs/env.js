c54f2b96d8b89a276589bc34411e63374ac65d98
Roan de Oliveira <roanrobersson@gmail.com>
2022-05-03 16:47:36 -0300

    fix: correct env variables

--- src/configs/env.js
+++ src/configs/env.js
@@ -7,16 +7,16 @@ export const AUTH_URL =
   "https://todoist.com/oauth/authorize";
 
 export const AUTH_SCOPE =
-  import.meta.env.VITE_RSHOP_AUTH_SCOPE?.toString() ??
+  import.meta.env.VITE_TODOIST_AUTH_SCOPE?.toString() ??
   "data:read_write,data:delete,project:delete";
 
 export const AUTH_STATE =
-  import.meta.env.VITE_RSHOP_AUTH_CODE?.toString() ?? "compass.uol";
+  import.meta.env.VITE_TODOIST_AUTH_CODE?.toString() ?? "compass.uol";
 
 export const CLIENT_ID =
-  import.meta.env.VITE_RSHOP_CLIENT_ID?.toString() ??
+  import.meta.env.VITE_TODOIST_CLIENT_ID?.toString() ??
   "c11c6b6e75cf49a191217deef00c3358";
 
 export const CLIENT_SECRET =
-  import.meta.env.VITE_RSHOP_CLIENT_SECRET?.toString() ??
+  import.meta.env.VITE_TODOIST_CLIENT_SECRET?.toString() ??
   "3babbf94e68648a284e1ab24d9ece14b";
